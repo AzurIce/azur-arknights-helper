a522580414dc5478fa0a55c4531d159a38a06d12
Azur冰弦 <973562770@qq.com>
2023-12-29 12:55:36 +0800

    initial commit

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "demo-iced";
+
+  nixConfig = {
+    extra-substituters = [
+      "https://mirrors.ustc.edu.cn/nix-channels/store"
+    ];
+    trusted-substituters = [
+      "https://mirrors.ustc.edu.cn/nix-channels/store"
+    ];
+  };
+
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            # clang
+            # llvmPackages_16.bintools
+            # openssl
+            # pkg-config
+            rust-bin.beta.latest.default
+          # # ] ++ (with pkgs.darwin.apple_sdk.frameworks; pkgs.lib.optionals pkgs.stdenv.isDarwin [
+          # #   System
+          # #   IOKit
+          # #   Security
+          # #   CoreFoundation
+          # #   AppKit
+          # ]);
+          ];
+        };
+      }
+    );
+}
