b90470bcd4d2544a283ccec71f98c4c9a4f5c7c8
Azur冰弦 <973562770@qq.com>
2024-06-30 15:51:34 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,8 @@ fn main() {
     if let Some(task) = cli.task {
         let serial = cli.serial_number.unwrap_or("127.0.0.1:16384".to_string());
         let aah = AAH::connect(serial, "./resources").expect("failed to connect to the device");
-        aah.run_task(task).expect("failed to execute task");
+        if let Err(err) = aah.run_task(task) {
+            println!("task failed: {err}")
+        }
     }
 }
