cbf0918dc1000f5d7fb2a0e8dbe9a8a890fff85f
Azur冰弦 <973562770@qq.com>
2024-07-06 17:47:46 +0800

    feat: template_matching cross_correlation and normed

--- packages/aah-core/src/vision/matcher/best_matcher.rs
+++ packages/aah-core/src/vision/matcher/best_matcher.rs
@@ -1,16 +1,54 @@
+use std::time::Instant;
+
+use aah_cv::ccoeff_normed;
+use color_print::{cformat, cprintln};
 use image::DynamicImage;
+use imageproc::template_matching::{find_extremes, match_template_parallel, MatchTemplateMethod};
 
-pub struct BestMatcherResult {
-
-}
+pub struct BestMatcherResult {}
 
 pub struct BestMatcher {
     images: Vec<DynamicImage>,
-    template: DynamicImage,
 }
 
 impl BestMatcher {
-    pub fn new(images: Vec<DynamicImage>, template: DynamicImage) -> Self {
-        Self { images, template }
+    pub fn new(images: Vec<DynamicImage>) -> Self {
+        Self { images }
+    }
+
+    pub fn match_with(&self, template: DynamicImage) -> usize {
+        let log_tag = cformat!("[BestMatcher]: ");
+        cprintln!(
+            "<dim>{log_tag}matching template with {} images</dim>",
+            self.images.len()
+        );
+
+        let t = Instant::now();
+        let res = self
+            .images
+            .iter()
+            .map(|img| {
+                // cprintln!(
+                //     "<dim>matching {}x{} with {}x{}...</dim>",
+                //     img.width(),
+                //     img.height(),
+                //     template.width(),
+                //     template.height()
+                // );
+                // let res = ccoeff_normed(&img.to_luma32f(), &template.to_luma32f());
+                let res = match_template_parallel(
+                    &img.to_luma8(),
+                    &template.to_luma8(),
+                    MatchTemplateMethod::CrossCorrelationNormalized,
+                );
+                let extremes = find_extremes(&res);
+                // println!("{:?}", extremes);
+                extremes.max_value
+            })
+            .enumerate()
+            .max_by(|a, b| a.1.partial_cmp(&b.1).unwrap())
+            .unwrap();
+        cprintln!("<dim>cost: {:?}</dim>", t.elapsed());
+        res.0
     }
 }
---
+++ packages/aah-cv/src/template_matching/shaders/cross_correlation_normed.wgsl
@@ -0,0 +1,61 @@
+struct Uniforms {
+    input_width: u32,
+    input_height: u32,
+    template_width: u32,
+    template_height: u32,
+    template_sq_sum: f32,
+};
+
+@group(0)
+@binding(0)
+var<storage, read> input_buf: array<f32>;
+
+@group(0)
+@binding(1)
+var<storage, read> template_buf: array<f32>;
+
+@group(0)
+@binding(2)
+var<storage, read_write> result_buf: array<f32>;
+
+@group(0)
+@binding(3)
+var<uniform> uniforms: Uniforms;
+
+@compute
+@workgroup_size(16, 16, 1)
+// Cross Correleation
+fn main(@builtin(global_invocation_id) global_id: vec3<u32>) {
+    var x = global_id.x;
+    var y = global_id.y;
+
+    var input_width = uniforms.input_width;
+    var input_height = uniforms.input_height;
+
+    var template_width = uniforms.template_width;
+    var template_height = uniforms.template_height;
+
+    var match_width = min(template_width, input_width - x);
+    var match_height = min(template_height, input_height - y);
+
+    var total_sum = 0.0;
+    var input_sq_sum = 0.0;
+    for (var i = 0u; i < match_width; i++) {
+        for (var j = 0u; j < match_height; j++) {
+            var input_idx = (y + j) * input_width + (i + x);
+            var template_idx = j * template_width + i;
+
+            var input_val = input_buf[input_idx];
+            var template_val = template_buf[template_idx];
+
+            var cc = input_val * template_val;
+            input_sq_sum += input_val * input_val;
+
+            total_sum += cc;
+        }
+    }
+    total_sum /= sqrt(input_sq_sum * uniforms.template_sq_sum);
+
+    var result_idx = y * (input_width - template_width + 1u) + x;
+    result_buf[result_idx] = total_sum;
+}
