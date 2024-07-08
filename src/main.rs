8e7e43fbdc780621a87a9f046ae7c2dbead3b0cf
Azur冰弦 <973562770@qq.com>
2024-07-08 13:23:53 +0800

    use sqdif for single match

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,55 @@
 #![feature(path_file_prefix)]
 
 use aah_core::AAH;
-use clap::Parser;
+use clap::{CommandFactory, Parser, Subcommand};
 
 #[derive(Parser)]
 #[command(author, version, about)]
 struct Cli {
-    /// The serial number of the target device
+    /// The serial number of the target device, default: 127.0.0.1:16384
     #[arg(short, long)]
     serial_number: Option<String>,
 
     /// The task name want to execute
-    task: Option<String>,
+    #[command(subcommand)]
+    task: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// run task
+    Task {
+        /// task name
+        name: String,
+    },
+    /// run copilot
+    Copilot {
+        ///copilot name
+        name: String,
+    },
 }
 
 fn main() {
     let cli = Cli::parse();
 
-    if let Some(task) = cli.task {
-        let serial = cli.serial_number.unwrap_or("127.0.0.1:16384".to_string());
-        let aah =
-            AAH::connect(serial, "./resources", |_| {}).expect("failed to connect to the device");
-        if let Err(err) = aah.run_task(task) {
-            println!("task failed: {err}")
+    let serial = cli.serial_number.unwrap_or("127.0.0.1:16384".to_string());
+    if cli.task.is_none() {
+        Cli::command().print_help().unwrap();
+        return;
+    }
+
+    let command = cli.task.as_ref().unwrap();
+    let aah = AAH::connect(serial, "./resources", |_| {}).expect("failed to connect to the device");
+    match command {
+        Commands::Task { name } => {
+            if let Err(err) = aah.run_task(name) {
+                println!("task failed: {err}")
+            }
+        }
+        Commands::Copilot { name } => {
+            if let Err(err) = aah.run_copilot(name) {
+                println!("copilot failed: {err}")
+            }
         }
     }
 }
