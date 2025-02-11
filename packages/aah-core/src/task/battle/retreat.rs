f143a24cbe757d177e77d14ae9597751a1977930
AzurIce <973562770@qq.com>
2025-02-11 12:08:44 +0800

    refactor: renamed some methods

--- packages/aah-core/src/task/battle/retreat.rs
+++ packages/aah-core/src/task/battle/retreat.rs
@@ -8,9 +8,9 @@ impl Retreat {
         let tile_pos = level.calc_tile_screen_pos(tile_pos.0, tile_pos.1, false);
         let retreat_pos = level.get_retreat_screen_pos();
         let task = Task::from_steps(vec![
-            TaskStep::action(Click::new(tile_pos.0 as u32, tile_pos.1 as u32)),
-            TaskStep::action(Click::new(retreat_pos.0 as u32, retreat_pos.1 as u32))
-                .deplay_sec_f32(0.2),
+            TaskStep::from_action(Click::new(tile_pos.0 as u32, tile_pos.1 as u32)),
+            TaskStep::from_action(Click::new(retreat_pos.0 as u32, retreat_pos.1 as u32))
+                .with_delay(0.2),
         ])
         .with_name("Retreat");
         task
